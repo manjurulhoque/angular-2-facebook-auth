924b33f3686fb3b7f7601c0390b0262da884cd75
manjurulhoque <rumimanzurulhoque@gmail.com>
2017-04-17 16:51:29 +0600

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from './providers/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  private isLoggedIn: Boolean;
+  private username: String;
+  private email: String;
+  constructor(public authService: AuthService, private router: Router) {
+    this.authService.af.auth.subscribe((auth) => {
+      if(auth == null)
+      {
+        this.isLoggedIn = false;
+        this.username = "";
+        this.email = "";
+        this.router.navigate(['login']);
+      }
+      else
+      {
+        this.isLoggedIn = true;
+        this.username = auth.google.displayName;
+        this.email = auth.google.email;
+        this.router.navigate(['']);
+      }
+    })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,44 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { RouterModule, Routes } from '@angular/router';
+
+import { AngularFireModule } from 'angularfire2';
 import { AppComponent } from './app.component';
+
+import { AuthService } from './providers/auth.service';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
+
+export const firebaseConfig = {
+  apiKey: "key",
+  authDomain: "authDomain",
+  databaseURL: "databaseURL",
+  projectId: "id",
+  storageBucket: "storageBucket",
+  messagingSenderId: "id"
+};
+
+const routes: Routes = [
+  {path: '', component: HomePageComponent},
+  {path: 'login', component: LoginPageComponent}
+]
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
